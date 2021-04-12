e62326b4cd53dd078eb4297fe734a94cf1092045
Sascha <sascha.fluor@gmx.net>
2021-04-12 08:08:22 +0200

    small changes

--- ReloadButtonErweiterungFSA.js
+++ ReloadButtonErweiterungFSA.js
@@ -57,6 +57,8 @@ define(["jquery", "qlik", "text!./lib/css/style.css"], function($, qlik, cssCont
 
 			function getLastDurationTime(){
 				var reloadTime;
+				var storedValue = JSON.parse(localStorage.getItem("duration"))
+				console.log("stored Value from function: " + storedValue)
 				if (app.id === storedValue.id) {
 					reloadTime = storedValue.durationTime/1000
 				}
